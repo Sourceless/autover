829271a88aac88f378965192a846c30995668075
Laurence Pakenham-Smith <laurence@sourceless.org>
2021-02-13 17:15:52 +0000

    Initial little bit of git2 code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use git2::Repository;
+
 fn main() {
-    println!("Hello, world!");
+    let repo = match Repository::discover(".") {
+        Ok(repo) => repo,
+        Err(e) => panic!("Failed to open git repository: {}", e),
+    };
+
+    let head = repo.head().unwrap();
+    let name = head.name().unwrap();
+
+    println!("{}", name);
+
+    // TODO: get commits, and recurse down popping any relevant notes on to a
+    //       stack, as well as noting any merge commits, because we want to
+    //       count them as well
 }
