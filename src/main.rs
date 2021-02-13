a2d170006d34fe98b2df0742cf34d4011019bb20
Laurence Pakenham-Smith <laurence@sourceless.org>
2021-02-13 17:42:24 +0000

    Starts of a basic tree traversal

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use git2::Repository;
+use git2::{Repository, Commit, Oid};
 
 fn main() {
     let repo = match Repository::discover(".") {
@@ -14,4 +14,19 @@ fn main() {
     // TODO: get commits, and recurse down popping any relevant notes on to a
     //       stack, as well as noting any merge commits, because we want to
     //       count them as well
+    let top_commit = head.peel_to_commit().expect(&format!("Couldn't find a commit on ref {}", name)[..]);
+    let mut version_stack = Vec::new();
+
+    traverse_commits(&top_commit, &mut version_stack);
+}
+
+fn traverse_commits(commit: &Commit, version_stack: &mut Vec<Oid>) {
+    let commit_id = commit.id();
+    version_stack.push(commit_id);
+
+    let parents = commit.parents();
+
+    for parent in parents {
+        println!("{:?}", parent);
+    }
 }
